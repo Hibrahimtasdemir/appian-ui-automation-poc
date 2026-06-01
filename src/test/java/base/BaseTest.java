da0fd71b70a62311a4783d7fcc5b949d89c66866
Halil Ibrahim Tasdemir <halil.tasdemir@bechtle.com>
2026-06-01 13:39:59 +0200

    Add screenshot capture on test failure

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,14 +1,23 @@
 package base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.ConfigReader;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BaseTest {
 
@@ -37,9 +46,39 @@ public class BaseTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            takeScreenshot(result.getMethod().getMethodName());
+        }
+
         if (driver != null) {
             driver.quit();
+        }
+    }
+
+    private void takeScreenshot(String testName) {
+        if (driver == null) {
+            return;
+        }
+
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+        String screenshotName = testName + "_" + timestamp + ".png";
+
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File destination = new File("target/screenshots/" + screenshotName);
+        destination.getParentFile().mkdirs();
+
+        try {
+            Files.copy(
+                    screenshot.toPath(),
+                    destination.toPath(),
+                    StandardCopyOption.REPLACE_EXISTING
+            );
+
+            System.out.println("Screenshot saved to: " + destination.getAbsolutePath());
+
+        } catch (IOException e) {
+            throw new RuntimeException("Could not save screenshot: " + destination.getAbsolutePath(), e);
         }
     }
 }

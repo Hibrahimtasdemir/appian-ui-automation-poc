2e5cdae48f0518da6ede8abc87067cea7d671cbd
Halil Ibrahim Tasdemir <halil.tasdemir@bechtle.com>
2026-06-03 16:37:22 +0200

    Reuse browser session across validation suite

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,24 +4,28 @@ import drivers.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import utils.ScreenshotUtils;
 
 public class BaseTest {
 
-    protected WebDriver driver;
+    protected static WebDriver driver;
 
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() {
+    @BeforeSuite(alwaysRun = true)
+    public void setUpSuite() {
         driver = DriverFactory.createDriver();
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown(ITestResult result) {
+    public void takeScreenshotOnFailure(ITestResult result) {
         if (result.getStatus() == ITestResult.FAILURE) {
             ScreenshotUtils.takeScreenshot(driver, result.getMethod().getMethodName());
         }
+    }
 
+    @AfterSuite(alwaysRun = true)
+    public void tearDownSuite() {
         if (driver != null) {
             driver.quit();
         }
